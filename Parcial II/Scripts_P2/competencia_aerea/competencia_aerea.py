41fdce210bfb05178142b59a276d7cc03d1372fe
Alejandro Bolívar <bolivara@gmail.com>
2023-06-25 07:19:42 -0400

    Add files via upload

---
+++ Parcial II/Scripts_P2/competencia_aerea/competencia_aerea.py
@@ -0,0 +1,24 @@
+'''
+Se encontraban Phineas y Ferb viendo un programa en el canal de documentales,
+cuando Phineas dice: Ferb, ¡ya se que vamos a hacer hoy!
+Así fue como se les ocurrio la sorprendente idea de realizar una carrera en aeronaves
+con sus amigos. Como es de suponer, no podian pedirles aviones de verdad a sus
+padres, asi que decidieron diseñar y ensamblar sus propios aviones y los de sus amigos.
+Antes de iniciar la carrera, declararon que Perry el ornitorrinco sería el juez encargado
+de anunciar al ganador. Perry se aprovechó de su tecnología de super agente secreto y
+colocó un dispositivo rastreador GPS en cada avión participante.
+Al final de la competencia, los rastreadores proporcionarán los siguientes datos, para
+cada uno de los aviones participantes:
+IDENTIFICACIÓN DEL AVIÓN, DISTANCIA TOTAL RECORRIDA (EXPRESADA EN KM) Y TIEMPO DE DURACIÓN DEL VUELO (EXPRESADO EN
+MINUTOS)
+Esta información se encuentra almacenada en el archivo competidores.txt. Por lo que, Perry necesita que usted desarrolle un programa en que lea los datos del archivo, y determine e imprima en otro archivo de nombre resultados.txt lo siguiente:
+Para cada competidor:
+ Identificación del avión, un mensaje que indique si llegó a la meta o no, y
+ Velocidad de vuelo del avión expresada en Km/h.
+Para todos los competidores:
+ Porcentaje de competidores que no llegaron a la meta,
+ Identificación del avión ganador.
+Consideraciones:
+ No todos los aviones pudieron llegar a la meta por defectos técnicos.
+ La meta se encuentra a 4600 Km del punto de partida y el ganador será aquel que haya recorrido una distancia mayor o igual a 4600 Km con la mayor velocidad.
+'''
